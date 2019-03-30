90d33fd234dad01d9f0c1ca6af76c8552c6805e0
Gyorgy Rethy <rethygy0410@gmail.com>
2019-03-30 17:16:59 +0000

    Separate game engine from main.js

--- src/main.js
+++ src/main.js
@@ -1,35 +1,8 @@
 'use strict';
 
-import * as PIXI from 'pixi.js';
+//import * as PIXI from 'pixi.js';
+import { GameObject } from "./scripts/game-engine.js"
 
-export class GameObject {
-    constructor(sprites, xPos, yPos, loaded){
-        let instance = this;
-        instance.pos = new PIXI.Point(xPos, yPos);
-        PIXI.Loader.shared.add(sprites).load(function() {
-            instance.sprites = new PIXI.AnimatedSprite(
-                PIXI.Loader.shared.resources[sprites].spritesheet.animations["dead"]);
-
-            instance.sprites.pivot.x = instance.sprites.pivot.y = 0.5;
-            instance.sprites.scale.x = instance.sprites.scale.y = 0.2;
-            instance.sprites.position = instance.pos;
-
-            if (typeof loaded === "function") {
-                loaded(instance);
-            }
-            else {
-                console.error("GameObject: loaded should be a function!");
-            }
-        });
-
-    }
-
-    display(stage) {
-        this.sprites.animationSpeed = 0.167;
-        this.sprites.play();
-        stage.addChild(this.sprites);
-    }
-}
 
 const app = new PIXI.Application({
     width: 512,
@@ -39,6 +12,8 @@ const app = new PIXI.Application({
 
 document.querySelector("#GameView").appendChild(app.view);
 
-let aThing = new GameObject("animation/spritesheet.json", 0, 0, function(thingy) {
-    thingy.display(app.stage);
+window.aThing = new GameObject(app.stage, "animation/spritesheet.json", function(thingy) {
+    thingy.setPosition(0, 0);
+    thingy.setScale(0.2);
+    thingy.display();
 });
