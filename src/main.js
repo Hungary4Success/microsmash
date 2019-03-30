9a12c63976a0caf799290f7dadc93b555266e1a9
Gyorgy Rethy <rethygy0410@gmail.com>
2019-03-30 16:05:31 +0000

    Base game object

--- src/main.js
+++ src/main.js
@@ -1,39 +1,41 @@
 'use strict';
 
 class GameObject {
-	constructor(sprites, xPos, yPos, loaded){
-		let instance = this;
-		PIXI.loader.add(sprites).load(function() {
-			instance.sprites = PIXI.loader.resources[sprites];
-		});
-		instance.pos = new Position(xPos, yPos);
+    constructor(sprites, xPos, yPos, loaded){
+        let instance = this;
+        instance.pos = new Position(xPos, yPos);
+        PIXI.Loader.shared.add(sprites).load(function() {
+            instance.sprites = PIXI.Loader.shared.resources[sprites];
+            if (typeof loaded === "function") {
+                loaded(instance);
+            }
+            else {
+                console.error("GameObject: loaded should be a function!");
+            }
+        });
 
-		if (typeof loaded === "function") {
-			loaded();
-		}
-		else {
-			console.error("GameObject: loaded should be a function!");
-		}
-	}
+    }
 
-	display(stage) {
-		stage.addChild(new PIXI.Sprite(this.sprites.texture));
-	}
+    display(stage) {
+        stage.addChild(new PIXI.Sprite(this.sprites.texture));
+    }
 }
 
 class Position {
-	constructor(xPos, yPos) {
-		this.x = xPos;
-		this.y = yPos;
-	}
+    constructor(xPos, yPos) {
+        this.x = xPos;
+        this.y = yPos;
+    }
 }
 
 const app = new PIXI.Application({
-	width: 512,
-	height: 512,
-	antialias: true
+    width: 512,
+    height: 512,
+    antialias: true
 });
 
 document.querySelector("#GameView").appendChild(app.view);
 
-//let aThing = new GameObject("", 0, 0);
+let aThing = new GameObject("attack _01.png", 0, 0, function(thingy) {
+    thingy.display(app.stage);
+});
