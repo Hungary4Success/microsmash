e5d19254229e4717293bbb54178ea5dce6e31133
Laszlo Nagy <laszlo.nagy@student.manchester.ac.uk>
2019-03-30 22:19:46 +0000

    Changes labels

--- microcode/controller.py
+++ microcode/controller.py
@@ -10,20 +10,20 @@ while True:
     # Jumping
     if height > 0:
         display.show(Image.ARROW_N)
-        print(str(controllerId) + ",jump;", flush=True)
+        print(str(controllerId) + ",JUMP;", flush=True)
     # Tilting:
     elif tilt > 400:
         display.show("R")
-        print(str(controllerId) + ",right_tilt," + str(tilt) + ";", flush=True)
+        print(str(controllerId) + ",RIGHT," + str(tilt) + ";", flush=True)
     elif tilt < -400:
         display.show("L")
-        print(str(controllerId) + ",left_tilt," + str(tilt) + ";", flush=True)
+        print(str(controllerId) + ",LEFT," + str(tilt) + ";", flush=True)
     # Buttons
     elif button_a.is_pressed():
         display.show(Image.PITCHFORK)
-        print(str(controllerId) + ",attack;", flush=True)
+        print(str(controllerId) + ",ATTACK;", flush=True)
     elif button_b.is_pressed():
         display.show(Image.TRIANGLE_LEFT)
-        print(str(controllerId) + ",defend;", flush=True)
+        print(str(controllerId) + ",DEFENSE;", flush=True)
 
     sleep(100)
