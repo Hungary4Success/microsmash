061bf3d57a4a34bad5b3b482fd4eba86929bf7e3
András Geiszl <geiszla@gmail.com>
2019-03-30 17:19:11 +0000

    Move main.js to scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,19 @@
 'use strict';
 
 import './external/pixi.min.js';
+import { GameObject } from "./scripts/game-engine.js"
 
-class GameObject {
-	constructor(sprites, xPos, yPos, loaded){
-		let instance = this;
-		PIXI.loader.add(sprites).load(function() {
-			instance.sprites = PIXI.loader.resources[sprites];
-		});
-		instance.pos = new Position(xPos, yPos);
-
-		if (typeof loaded === "function") {
-			loaded();
-		}
-		else {
-			console.error("GameObject: loaded should be a function!");
-		}
-	}
-
-	display(stage) {
-		stage.addChild(new PIXI.Sprite(this.sprites.texture));
-	}
-}
-
-class Position {
-	constructor(xPos, yPos) {
-		this.x = xPos;
-		this.y = yPos;
-	}
-}
 
 const app = new PIXI.Application({
-	width: 512,
-	height: 512,
-	antialias: true
+    width: 512,
+    height: 512,
+    antialias: true
 });
 
 document.querySelector("#GameView").appendChild(app.view);
 
-//let aThing = new GameObject("", 0, 0);
+window.aThing = new GameObject(app.stage, "animation/spritesheet.json", function(thingy) {
+    thingy.setPosition(0, 0);
+    thingy.setScale(0.2);
+    thingy.display();
+});
