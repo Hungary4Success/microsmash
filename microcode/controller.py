6ef1ebe5bdf51709a5b3448b69e20d4b5371e5df
Laszlo Nagy <laszlo.nagy@student.manchester.ac.uk>
2019-03-30 16:44:33 +0000

    Adds printing

--- microcode/controller.py
+++ microcode/controller.py
@@ -5,16 +5,21 @@ while True:
     tilt = accelerometer.get_x()
     if tilt > 400:
         display.show("R")
+        print("right_tilt")
     elif tilt < -400:
         display.show("L")
+        print("left_tilt")
 
     # Jumping
     height = accelerometer.get_z()
     if height > 0:
         display.show(Image.ARROW_N)
+        print("jump")
 
     # Buttons
     if button_a.is_pressed():
         display.show(Image.PITCHFORK)
+        print("attack")
     elif button_b.is_pressed():
         display.show(Image.TRIANGLE_LEFT)
+        print("defend")
