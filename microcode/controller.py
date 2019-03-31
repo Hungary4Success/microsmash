d2b830ba699335fbe39e3be3e91733e41ec2b561
Laszlo Nagy <laszlo.nagy@student.manchester.ac.uk>
2019-03-31 10:08:40 +0100

    Minor adjustments

--- microcode/controller.py
+++ microcode/controller.py
@@ -8,22 +8,22 @@ while True:
     height = accelerometer.get_z()
 
     # Jumping
-    if height > 0:
+    if height > -400:
         display.show(Image.ARROW_N)
         print(str(controllerId) + ",JUMP;", flush=True)
     # Tilting:
-    elif tilt > 0:
+    elif tilt > 200:
         display.show("R")
         print(str(controllerId) + ",RIGHT," + str(tilt) + ";", flush=True)
-    elif tilt < 0:
+    elif tilt < -200:
         display.show("L")
         print(str(controllerId) + ",LEFT," + str(tilt) + ";", flush=True)
     # Buttons
     elif button_a.is_pressed():
-        display.show(Image.PITCHFORK)
-        print(str(controllerId) + ",ATTACK;", flush=True)
-    elif button_b.is_pressed():
         display.show(Image.TRIANGLE_LEFT)
         print(str(controllerId) + ",DEFENSE;", flush=True)
+    elif button_b.is_pressed():
+        display.show(Image.PITCHFORK)
+        print(str(controllerId) + ",ATTACK;", flush=True)
 
     sleep(200)
